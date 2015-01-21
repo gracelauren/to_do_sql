a207fb89d1cdb6bd4849016a0033f9f9d1764b76
Grace <gracelauren909@gmail.com>
2015-01-20 20:15:44 -0800

    Add delete and rename options, epicodus homework

--- app.rb
+++ app.rb
@@ -33,3 +33,22 @@ post("/tasks") do
   @list = List.find(list_id)
   erb(:list)
 end
+
+get("/lists/:id/edit") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list_edit)
+end
+
+patch("/lists/:id") do
+  name = params.fetch("name")
+  @list = List.find(params.fetch("id").to_i())
+  @list.update({:name => name})
+  erb(:list)
+end
+
+delete("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  @list.delete()
+  @lists = List.all()
+  erb(:index)
+end
